a35ee332288a0d61ea1911ca6b8ed56819e8b755
AbdulHameedi <sanilinity507@gmail.com>
2024-08-06 21:56:27 +0100

    main

---
+++ assets/js/component/subscribe.js
@@ -0,0 +1,14 @@
+// window.onload=()=>{
+    // function showSubsribe(event){    
+    //     document.getElementsByClassName('notify').classList.remove('hidden')
+    //     console.log(event)
+    // }
+    // showSubsribe()
+// }
+
+
+// window.onload = function() {
+//     setTimeout(function() {
+//         document.getElementsByClassName('notify').classList.remove('hidden');
+//     }, 3000); // 10000 milliseconds = 10 seconds
+// };
